b2b79b5e7d8dc4f6762ea7855be1d36e103369a9
Peacoock-hub <profesorjmms@gmail.com>
2025-03-18 20:30:11 -0600

    Actualizacion de mi proyecto

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Tripleten web_project_around
+https://github.com/Peacoock-hub/web_project_around
---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let butEdit = document.querySelector(".main__button_edit");
+let popup = document.querySelector(".popup");
+let butClose = document.querySelector(".popup__button_close");
+let form = document.querySelector(".popup__container");
+let inName = document.querySelector(".main__paragraph_name");
+let inAbout = document.querySelector(".main__paragraph_about");
+let inpName = document.querySelector(".popup__input_name");
+let inpAbout = document.querySelector(".popup__input_about");
+
+function openEdit() {
+  inpName.value = inName.textContent;
+  inpAbout.value = inAbout.textContent;
+  popup.classList.toggle("popup_opened");
+}
+
+butEdit.addEventListener("click", openEdit);
+butClose.addEventListener("click", openEdit);
+
+function saveChange(e) {
+  e.preventDefault();
+  inName.textContent = inpName.value;
+  inAbout.textContent = inpAbout.value;
+  openEdit();
+}
+if (nameValue !== "" && aboutValue !== "") {
+  profileName.textContent = nameValue;
+  profileAbout.textContent = aboutValue;
+  popupProfile.classList.remove("popup_opened");
+}
+
+form.addEventListener("submit", saveChange);
